983a23221e15118b265dd852ee5885d3eae5f368
bayed-saad <bayed.saad.sb@gmail.com>
2020-07-11 17:39:19 +0100

    uploadMovieApp

---
+++ API/TMDBApi.js
@@ -0,0 +1,20 @@
+const API_TOKEN = "6fb4e802aef57a0b95ff4e342652c6cd";
+export function getFilmsFromApiWithSearchedText (text, page) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
+    return fetch(url)
+      .then((response) => response.json())
+      .catch((error) => console.error(error))
+
+  }
+  // API/TMDBApi.js
+
+export function getImageFromApi (name) {
+    return 'https://image.tmdb.org/t/p/w300' + name
+  }
+
+  // Récupération du détail d'un film
+export function getFilmDetailFromApi (id) {
+  return fetch('https://api.themoviedb.org/3/movie/' + id + '?api_key=' + API_TOKEN + '&language=fr')
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
+}
